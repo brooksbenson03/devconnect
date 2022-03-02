31251b95a32fcd263a0056aeaab98c9d2f229111
Brooks Benson <brooksbenson03@gmail.com>
2022-03-02 12:07:10 -0700

    init

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('API running')
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+})
